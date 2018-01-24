5452bc0172fd0a2d50fc9acd9048291e6b517e3e
alemontiels <35465549+alemontiels@users.noreply.github.com>
2018-01-23 22:20:55 -0600

    Meta 1: ST con marcadores de Hospitals en Denpasar
    Ahora ST muestra un mapa con markers de hospitales en Denpasar, en vez de los puntos en EUA

---
+++ ST_with_markers_hospitals_Denpasar/server.R
@@ -0,0 +1,136 @@
+library(leaflet)
+library(RColorBrewer)
+library(scales)
+library(lattice)
+library(dplyr)
+library(rsconnect)
+
+function(input, output, session) {
+  
+  ## chooser
+  output$selection <- renderPrint(
+    input$mychooser
+  )
+  
+##------------------  UPLOADING FILE ------------------------------
+  
+  output$contents <- renderTable({
+    
+  req(input$file1)
+    
+    df <- read.csv(input$file1$datapath,
+                   header = input$header,
+                   sep = input$sep,
+                   quote = input$quote)
+    
+    if(input$disp == "head") {
+      return(head(df))
+    }
+    else {
+      return(df)
+    }
+    
+  })
+  #####------------------ DOwnloading
+  
+  # Reactive value for selected dataset ----
+  datasetInput <- reactive({
+    switch(input$dataset,
+           "Datos de superzip en EUA" = cars)
+  })
+  
+  # Table of selected dataset ----
+  output$table <- renderTable({
+    datasetInput()
+  })
+  
+  # Downloadable csv of selected dataset ----
+  output$downloadData <- downloadHandler(
+    filename = function() {
+      paste(input$dataset, ".csv", sep = "")
+    },
+    content = function(file) {
+      write.csv(datasetInput(), file, row.names = FALSE)
+    }
+  )
+  
+# Create the map
+  output$map <- renderLeaflet({
+    leaflet() %>%
+      addTiles(
+     urlTemplate = "//{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png"
+      #  attribution = 'Maps by <a href="http://www.mapbox.com/">Mapbox</a>'
+      ) %>%
+      setView(lng = 115.25942542764824, lat =-8.645581443331796, zoom = 11)
+})
+  
+  # icon marker
+  markerhosp <- makeIcon(
+    iconUrl = "http://cdn0.iconfinder.com/data/icons/healthcare-medicine/512/hospital_location-512.png",
+    iconWidth = 22, iconHeight = 27
+  )
+  
+  
+  leafletProxy("map", data = allzips) %>%
+     clearShapes() %>%
+   addMarkers(lng=~longitude, lat=~latitude,icon=~markerhosp, popup=~paste("<h6 style='color:red'>#",code,"</h6>","<strong>Hospital:</strong>",name))
+
+  ## Data Explorer  ###########################################
+
+  observe({
+    cities <- if (is.null(input$states)) character(0) else {
+      filter(cleantable, State %in% input$states) %>%
+        `$`('City') %>%
+        unique() %>%
+        sort()
+    }
+    stillSelected <- isolate(input$cities[input$cities %in% cities])
+    updateSelectInput(session, "cities", choices = cities,
+      selected = stillSelected)
+  })
+
+  observe({
+    zipcodes <- if (is.null(input$states)) character(0) else {
+      cleantable %>%
+        filter(State %in% input$states,
+          is.null(input$cities) | City %in% input$cities) %>%
+        `$`('Zipcode') %>%
+        unique() %>%
+        sort()
+    }
+    stillSelected <- isolate(input$zipcodes[input$zipcodes %in% zipcodes])
+    updateSelectInput(session, "zipcodes", choices = zipcodes,
+      selected = stillSelected)
+  })
+
+  observe({
+    if (is.null(input$goto))
+      return()
+    isolate({
+      map <- leafletProxy("map")
+      map %>% clearPopups()
+      dist <- 0.5
+      zip <- input$goto$zip
+      lat <- input$goto$lat
+      lng <- input$goto$lng
+      showZipcodePopup(zip, lat, lng)
+      map %>% fitBounds(lng - dist, lat - dist, lng + dist, lat + dist)
+    })
+  })
+
+  output$ziptable <- DT::renderDataTable({
+    df <- cleantable %>%
+      filter(
+        Score >= input$minScore,
+        Score <= input$maxScore,
+        is.null(input$states) | State %in% input$states,
+        is.null(input$cities) | City %in% input$cities,
+        is.null(input$zipcodes) | Zipcode %in% input$zipcodes
+      ) %>%
+      mutate(Action = paste('<a class="go-map" href="" data-lat="', Lat, '" data-long="', Long, '" data-zip="', Zipcode, '"><i class="fa fa-crosshairs"></i></a>', sep=""))
+    action <- DT::dataTableAjax(session, df)
+
+    DT::datatable(df, options = list(ajax = list(url = action)), escape = FALSE)
+  })
+}
+
